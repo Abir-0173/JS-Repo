54cb79400276057ee1590630dbf95c0466415190
Abir <tanimur196@gmail.com>
2024-02-08 14:45:08 +0600

    get api

--- js/FetchAPI.js
+++ js/FetchAPI.js
@@ -19,37 +19,87 @@
 //             output += `<li>${personName.firstName} ${personName.lastName}</li>`
 //         });
 //         document.getElementById('output').innerHTML = output;
-        
+
 //     }, 1000)
 // }
 //---------------------
 
 
-let persons = [
-    {firstName: "Tanimur", lastName: "Abir"},
-    {firstName: "Amzad", lastName: "Rahat"}
-]
-function createPerson(newPerson, callback) {
-    setTimeout(function() {
-        persons.push(newPerson);
-        callback();
-    }, 1000);
+// let persons = [
+//     {firstName: "Tanimur", lastName: "Abir"},
+//     {firstName: "Amzad", lastName: "Rahat"}
+// ]
+// function createPerson(newPerson, callback) {
+//     setTimeout(function() {
+//         persons.push(newPerson);
+//         callback();
+//     }, 1000);
+// }
+// function getPerson(params) {
+//     setTimeout(function () {
+//         let output = '';
+//         persons.forEach(function(personName){
+//             output += `<li>${personName.firstName} ${personName.lastName}</li>`
+//         });
+//         document.getElementById('output').innerHTML = output;
+
+//     }, 500)
+// }
+
+// createPerson({firstName:"SH", lastName:"Badhon"}, getPerson);
+//------------------------ooo----------ooo-------------oooo----------------
+
+
+//promises
+//.then
+
+// let persons = [
+//         {firstName: "Tanimur", lastName: "Abir"},
+//         {firstName: "Amzad", lastName: "Rahat"}
+//     ]
+//     function createPerson(newPerson, ) {
+//         let prom = new Promise(function (resolve, reject) {
+//             persons.push(newPerson);
+//             let error = false;
+
+//             if (!error){
+//                 resolve();
+//             }
+//             else{
+//                 reject('error! Somthing wrong!');
+//             }
+//         })
+//        return prom;
+//     }
+//     function getPerson(params) {
+//         setTimeout(function () {
+//             let output = '';
+//             persons.forEach(function(personName){
+//                 output += `<li>${personName.firstName} ${personName.lastName}</li>`
+//             });
+//             document.getElementById('output').innerHTML = output;
+
+//         }, 500)
+//     }
+
+//     createPerson({firstName:"SH", lastName:"Badhon"}).then(getPerson)
+//     .catch(function(err){
+//         console.log(err);
+//     })
+
+//----------------------------- Fatch API ---------------------
+// console.log(window);
+
+document.getElementById("get_data").addEventListener('click', getData);
+
+// xhr.open('GET', 'http://api.icndb.com/jokes/random/', true);
+
+function getData() {
+    fetch('https://randomuser.me/api/')
+    .then(function(res){
+        console.log(res);
+    })
 }
-function getPerson(params) {
-    setTimeout(function () {
-        let output = '';
-        persons.forEach(function(personName){
-            output += `<li>${personName.firstName} ${personName.lastName}</li>`
-        });
-        document.getElementById('output').innerHTML = output;
-        
-    }, 500)
-}
-
-createPerson({firstName:"SH", lastName:"Badhon"}, getPerson);
-
-
-
 
 
 

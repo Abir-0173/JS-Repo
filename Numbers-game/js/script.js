779cd1765fdd0a6ce92ad2fb66fad7fae217326c
Abir-0173 <tanimur196@gmail.com>
2024-05-03 01:17:04 +0600

    Numbers-game

---
+++ Numbers-game/js/script.js
@@ -0,0 +1,79 @@
+var min = 10;
+var max = 20;
+//quariselector
+
+let userInput = document.querySelector('#userInput');
+
+// console.log(userInput.value);
+
+document.querySelector('.submit').addEventListener("click", submit);
+var form = document.querySelector('.playAgain').addEventListener("click", reset);
+
+// console.log(x);
+
+//function
+function getRandomInt(min, max) {
+    const minCeiled = Math.ceil(min);
+    const maxFloored = Math.floor(max);
+    return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled); // The maximum is exclusive and the minimum is inclusive
+}
+
+var y = getRandomInt(min, max);
+console.log(y);
+let c = 0;
+
+function submit() {
+    var x = userInput.value;
+    c++;
+    console.log(c);
+
+    
+
+    if (x == y) {
+        alert('🎉🎉 Hurra ! You Win The game 🎉🎉');
+        // break;
+
+    } else if (x < y) {
+        alert(`Opps you are close 😱 It's Greter then ${x}`);
+        userInput.value.innerhtml = '';
+
+    } else if (x > y) {
+        alert(`Opps you are close to the number it's Less then ${x}`);
+        userInput.value.innerhtml = '';
+    }
+
+    // Check if the player has made 3 attempts
+    if (c >= 3) {
+        alert('☠️☠️ You Lose The GAME ☠️☠️');
+        y = getRandomInt(min, max);
+        console.log(y);
+        // Optionally, you can reset the game here
+        // reset();
+    }
+
+    // alert('🎉🎉 Hurra ! You Win The game 🎉🎉');
+    // break;
+    // if (i <= 3) {
+    //     alert('☠️☠️ You Loos The GAME ☠️☠️');
+    // }
+}
+// }
+// submit();
+
+
+
+
+
+
+function reset() {
+    document.querySelector('#userInput').value = '';
+    // console.log(userInput);
+    // console.log(userInput.value);
+    // var y = getRandomInt(min, max);
+    c = 0;
+    y = getRandomInt(min, max);
+    console.log(y);
+    // form.reset();
+    // userInput.innerHTML = "hi sdferfe fs";
+
+}
